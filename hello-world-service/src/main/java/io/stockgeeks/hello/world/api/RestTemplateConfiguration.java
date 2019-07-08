a4b9fa4ffa483c9686068c1d27339d48322d16d6
Tim <tim@jtim.nl>
2019-07-08 22:45:31 +0200

    Example Step 3 create RestTemplate using RestTemplateBuilder as Spring Bean

--- hello-world-service/src/main/java/io/stockgeeks/hello/world/api/RestTemplateConfiguration.java
+++ hello-world-service/src/main/java/io/stockgeeks/hello/world/api/RestTemplateConfiguration.java
@@ -1,5 +1,6 @@
 package io.stockgeeks.hello.world.api;
 
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
@@ -8,7 +9,7 @@ import org.springframework.web.client.RestTemplate;
 public class RestTemplateConfiguration {
 
     @Bean
-    public RestTemplate restTemplate() {
-        return new RestTemplate();
+    public RestTemplate restTemplate(RestTemplateBuilder restTemplateBuilder) {
+        return restTemplateBuilder.build();
     }
 }
